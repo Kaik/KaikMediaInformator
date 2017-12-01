2f6fe06c36edf63eb14f0aa60e35c5606f2c7ed0
Kaik <kapitanogor@googlemail.com>
2017-12-01 17:19:51 +0100

    Module basics to see it in extensions list.

---
+++ KaikmediaInformatorModule.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * KaikMedia InformatorModule
+ *
+ * @package    KaikmediaInformatorModule
+ * @author     Kaik <contact@kaikmedia.com>
+ * @copyright  KaikMedia
+ * @link       https://github.com/Kaik/KaikMediaInformator.git
+ */
+
+namespace Kaikmedia\InformatorModule;
+
+use Zikula\Core\AbstractModule;
+
+class KaikmediaInformatorModule extends AbstractModule
+{
+}
